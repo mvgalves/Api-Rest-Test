69cd548f1b88770f05b67872c10fe8b932642417
mvgalves <markitusalves2@gmail.com>
2025-05-09 00:52:45 -0300

    inicio do projeto

---
+++ bradesco-cloud-native/src/main/java/me/dio/bradesco_cloud_native/ApiRestApplication.java
@@ -0,0 +1,13 @@
+package me.dio.bradesco_cloud_native;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+	}
+
+}
